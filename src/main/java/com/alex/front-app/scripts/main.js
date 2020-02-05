3f4911b00c06cc10d8269c09172b74afe93a1b17
derwya <39012104+derwya@users.noreply.github.com>
2020-02-06 00:37:47 +0200

    Update main.js

--- src/main/java/com/alex/front-app/scripts/main.js
+++ src/main/java/com/alex/front-app/scripts/main.js
@@ -4,6 +4,7 @@ const logger = require("../modules/winston");
 const path = require("path");
 
 let window;
+let child_process;
 
 function createWindow() {
 
@@ -18,15 +19,15 @@ function createWindow() {
         maxHeight: 2160,
         fullscreenable: true,
         show: false,
-         icon: path.join(__dirname + "/../images/icon128.png"),
+         icon: path.join(__dirname, "..", "images", "icon128.png"),
         autoHideMenuBar: true,
         webPreferences: {
             nodeIntegration: true
         }
     });
 
-    // window.setMenu(null);
-     window.loadFile(path.join(__dirname + "/../pages/index.html"))
+    //window.setMenu(null);
+     window.loadFile(path.join(__dirname, "..", "pages", "index.html"))
         .catch(logger.error);
 
     logger.info("Window was created...");
@@ -46,22 +47,44 @@ function createWindow() {
 }
 
 
-app.once("ready", createWindow);
+app.once("ready", async () => {
+    child_process = await startServer();
+    await msleep(5000);
+    createWindow();
+});
 
 app.whenReady().then(() => {
     globalShortcut.register('CommandOrControl+N', () => {
         let child = new BrowserWindow({parent: window, width: 500, height:220, resizable: false, fullscreenable: false});
-        child.loadFile(path.join(__dirname + "/../pages/easter.html")).catch(logger.error);
+        child.loadFile(path.join(__dirname, "..", "pages", "easter.html")).catch(logger.error);
         child.setMenu(null);
         child.once("ready-to-show", () => child.show());
     });
 });
 
+
+
 app.once('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+    if (process.platform !== 'darwin') {
+        child_process.kill();
+        app.quit();
+    }
 });
+
 
 app.on('activate', () => {
     if (window === null) createWindow();
 });
 
+const msleep = time =>
+    new Promise(
+        resolve => setTimeout(_=>resolve(), time)
+    )
+;
+
+async function startServer() {
+    let spawn = require('child_process').spawn, child;
+    logger.info(__dirname);
+    child = spawn('java', ["-jar", (path.join(__dirname, "..", "server", "GUIApp-1.0-SNAPSHOT.jar").replace(/\s/g, "\ "))]);
+    return child;
+}
